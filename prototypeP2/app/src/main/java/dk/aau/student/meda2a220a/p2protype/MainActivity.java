96b5a1db2874ee297d236c40fd324cd009df7c6f
Emil Valentin <atomicpotato8@gmail.com>
2020-04-30 09:39:46 +0200

    first commit

---
+++ prototypeP2/app/src/main/java/dk/aau/student/meda2a220a/p2protype/MainActivity.java
@@ -0,0 +1,13 @@
+package dk.aau.student.meda2a220a.p2protype;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ prototypeP2/app/src/test/java/dk/aau/student/meda2a220a/p2protype/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package dk.aau.student.meda2a220a.p2protype;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
