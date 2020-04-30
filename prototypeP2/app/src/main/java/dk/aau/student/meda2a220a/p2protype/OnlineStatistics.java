455328e58d0183451f4fbd6b6cdd71db039e25ab
Sebastian <sebber280@live.dk>
2020-04-30 11:05:20 +0200

    Fixed activities so it has layout xml's

--- prototypeP2/app/src/main/java/dk/aau/student/meda2a220a/p2protype/OnlineStatistics.java
+++ prototypeP2/app/src/main/java/dk/aau/student/meda2a220a/p2protype/OnlineStatistics.java
@@ -1,4 +1,14 @@
 package dk.aau.student.meda2a220a.p2protype;
 
-public class OnlineStatistics {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnlineStatistics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_online_statistics);
+    }
 }
--- prototypeP2/app/src/main/java/dk/aau/student/meda2a220a/p2protype/Social.java
+++ prototypeP2/app/src/main/java/dk/aau/student/meda2a220a/p2protype/Social.java
@@ -1,4 +1,14 @@
 package dk.aau.student.meda2a220a.p2protype;
 
-public class Social {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Social extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_social);
+    }
 }
