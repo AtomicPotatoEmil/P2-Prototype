52f6b7a4e7589d0303ef020daccebffa88d7849b
Sebastian <sebber280@live.dk>
2020-05-04 15:42:30 +0200

    Game = Landscape orientation, Other = Portrait orientation

--- prototypeP2/app/src/main/java/dk/aau/student/meda2a220a/p2protype/Social.java
+++ prototypeP2/app/src/main/java/dk/aau/student/meda2a220a/p2protype/Social.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
