40e4811c344b66cb1408df8b3bf71ad7a6b82c63
Emil Valentin <atomicpotato8@gmail.com>
2020-05-04 11:17:45 +0200

    Made InitiateGameActivity

---
+++ prototypeP2/app/src/main/java/dk/aau/student/meda2a220a/p2protype/InitiateGameActivity.java
@@ -0,0 +1,17 @@
+package dk.aau.student.meda2a220a.p2protype;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class InitiateGameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_initiate_game);
+    }
+}
