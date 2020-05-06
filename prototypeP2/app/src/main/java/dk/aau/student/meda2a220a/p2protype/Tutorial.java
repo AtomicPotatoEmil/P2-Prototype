e14de21ceae8f730fde898facffd407be48e0f23
ThomasSTA <tsta19@student.aau.dk>
2020-05-06 13:51:11 +0200

    Added  new activity 'Tutorial'

---
+++ prototypeP2/app/src/main/java/dk/aau/student/meda2a220a/p2protype/Tutorial.java
@@ -0,0 +1,14 @@
+package dk.aau.student.meda2a220a.p2protype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tutorial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+    }
+}
