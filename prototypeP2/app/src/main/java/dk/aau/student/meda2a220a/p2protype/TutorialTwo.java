8202d0b175d8f612c357a554eebec10042f26144
ThomasSTA <tsta19@student.aau.dk>
2020-05-08 14:22:24 +0200

    Added second tutorial view

--- prototypeP2/app/src/main/java/dk/aau/student/meda2a220a/p2protype/TutorialTwo.java
+++ prototypeP2/app/src/main/java/dk/aau/student/meda2a220a/p2protype/TutorialTwo.java
@@ -8,19 +8,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 
-public class Tutorial extends AppCompatActivity {
+public class TutorialTwo extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tutorial);
+        setContentView(R.layout.activity_tutorialtwo);
 
-        Button button = (Button) findViewById(R.id.tutorialButtonNext);
+        Button button = (Button) findViewById(R.id.tutorialTwoButtonPlay);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Tutorial.this, TutorialTwo.class));
+                startActivity(new Intent(TutorialTwo.this, InitiateGameActivity.class));
             }
         });
 
