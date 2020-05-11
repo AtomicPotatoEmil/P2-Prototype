8b393a77ec955da1e1c073e34e5cd1d827e1d6ab
ThomasSTA <tsta19@student.aau.dk>
2020-05-11 16:00:39 +0200

    Completed GameSummary
    hype hype hype

--- prototypeP2/app/src/main/java/dk/aau/student/meda2a220a/p2protype/GameSummary.java
+++ prototypeP2/app/src/main/java/dk/aau/student/meda2a220a/p2protype/GameSummary.java
@@ -1,6 +1,8 @@
 package dk.aau.student.meda2a220a.p2protype;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,13 +17,22 @@ public class GameSummary extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gamesummary);
 
+        SharedPreferences prefs = getSharedPreferences("statistics", Context.MODE_PRIVATE);
+        int obstaclesDodged = prefs.getInt("obstaclesDodged", 0);
+        int obstaclesHit = prefs.getInt("obstaclesHit", 0);
+        int time = prefs.getInt("time", 0);
+
+
         // Buttons
         Button gameSummaryNextButton = (Button) findViewById(R.id.GameSummaryNextButton);
 
         // Recent Game Views
         TextView recentGameHitsView = (TextView) findViewById(R.id.RecentGameHitsView);
+        recentGameHitsView.setText(String.valueOf(obstaclesDodged));
         TextView recentGameDodgesView = (TextView) findViewById(R.id.RecentGameDodgesView);
+        recentGameDodgesView.setText(String.valueOf(obstaclesHit));
         TextView recentGameTimeView = (TextView) findViewById(R.id.RecentGameTimeView);
+        recentGameTimeView.setText(String.valueOf(time));
 
         // Best Game Views
         TextView bestGameHitsView = (TextView) findViewById(R.id.BestGameHitsView);
