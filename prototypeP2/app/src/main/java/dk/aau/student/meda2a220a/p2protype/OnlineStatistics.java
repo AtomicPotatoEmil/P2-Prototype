3b162dc0843dcab1bc57849577078610a6980f01
ThomasSTA <tsta19@student.aau.dk>
2020-05-12 14:18:29 +0200

    Added comments to OnlineStatistics

--- prototypeP2/app/src/main/java/dk/aau/student/meda2a220a/p2protype/OnlineStatistics.java
+++ prototypeP2/app/src/main/java/dk/aau/student/meda2a220a/p2protype/OnlineStatistics.java
@@ -13,47 +13,18 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class OnlineStatistics extends AppCompatActivity {
 
-    public static int numberOfCurrentGame = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online_statistics);
 
-
-        switch (numberOfCurrentGame) {
-            case 1:
-                Log.d("CASE 1", "CASE 1 WAS CHOSEN");
-                break;
-            case 2:
-                Log.d("CASE 2", "CASE 2 WAS CHOSEN");
-                break;
-            case 3:
-                Log.d("CASE 3", "CASE 3 WAS CHOSEN");
-                break;
-            case 4:
-                Log.d("CASE 4", "CASE 4 WAS CHOSEN");
-                break;
-            case 5:
-                Log.d("CASE 5", "CASE 5 WAS CHOSEN");
-                break;
-            case 6:
-                Log.d("CASE 6", "CASE 6 WAS CHOSEN");
-                break;
-            case 7:
-                Log.d("CASE 7", "CASE 7 WAS CHOSEN");
-                break;
-            default:
-                Log.d("SWITCH", "ERROR DEFAULT WAS CHOSEN");
-        }
-
+        // Handles the bottom navigation menu at the bottom of the application
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-
         bottomNavigationView.setSelectedItemId(R.id.OnlineStatistics);
-
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                // Switch statement to determine which page we are currently on
                 switch (menuItem.getItemId()) {
                     case R.id.OnlineStatistics:
                         return true;
